0775f2a25779d38c383b04fb42a05df4925241b3
Andres Felipe Pardo Mesa <aanfepar@gmail.com>
2021-01-12 09:58:36 -0500

    🚀 Feat: create initial dev settings

---
+++ src/views/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "../assets/styles/App.scss";
+
+const App = () => <h1>Hello World</h1>;
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    alias: {
+      "@/views": path.resolve(__dirname, "./src/views/"),
+      "@/components": path.resolve(__dirname, "./src/components/"),
+      "@/assets": path.resolve(__dirname, "./src/assets/"),
+    },
+    extensions: [".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|gif|jpg)$/,
+        use:[
+            {
+                "loader":"file-loader",
+                options: {
+                    name:"assets/[hash].[ext]"
+                }
+            }
+        ]
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].css",
+    }),
+  ],
+};
